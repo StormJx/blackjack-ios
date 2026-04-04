c02cb93a83651ed8dbde7b21bfcbb97e95ae6ab4
jixiang <2286607368@qq.com>
2026-04-04 21:34:20 +0800

    Initial Commit

---
+++ cards/cardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  cardsApp.swift
+//  cards
+//
+//  Created by 姬翔 on 2026/4/4.
+//
+
+import SwiftUI
+
+@main
+struct cardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ cardsTests/cardsTests.swift
@@ -0,0 +1,17 @@
+//
+//  cardsTests.swift
+//  cardsTests
+//
+//  Created by 姬翔 on 2026/4/4.
+//
+
+import Testing
+@testable import cards
+
+struct cardsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
